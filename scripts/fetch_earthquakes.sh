840d248f9b78b44ba376e82b283b22969b0ba62d
Simon Séhier <simon.sehier@camptocamp.com>
2025-03-07 12:52:59 +0100

    [IGI_SB-3752]: fix cache policy for earthquakes in download.swiwssgeol.ch bucket

--- scripts/fetch_earthquakes.sh
+++ scripts/fetch_earthquakes.sh
@@ -11,7 +11,7 @@ STARTTIME=$(date -d '-90 days' +'%FT%H:%M:%S')
 
 curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=1&format=text&nodata=404&EventType=earthquake" > $FILENAME
 
-aws s3 --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
+aws s3 --debug cp --cache-control max-age=86400 $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
 
 
 FILENAME="earthquakes_magnitude_gt_3.txt"
@@ -19,4 +19,4 @@ STARTTIME="1979-01-01T00:00:00"
 
 curl --fail "http://arclink.ethz.ch/fdsnws/event/1/query?starttime=$STARTTIME&endtime=$ENDTIME&minmagnitude=3&format=text&nodata=404&EventType=earthquake" > $FILENAME
 
-aws s3 --debug cp --cache-control no-cache $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
+aws s3 --debug cp --cache-control max-age=86400 $FILENAME s3://ngmpub-download-bgdi-ch/earthquakes/
