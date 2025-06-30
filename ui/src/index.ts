381fb279ea9374a89394857c565614aec5b98bfe
Daniel von Atzigen <daniel@vatz.ch>
2025-06-30 09:01:11 +0200

    Supress Lit dev warning

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -2,6 +2,8 @@ import '@swisstopo/swissgeol-ui-core/import';
 
 import './jquery.polyfill';
 import './style/index.css';
+
+import './lit.init';
 import { ReactiveElement } from 'lit';
 
 import './ngm-app-boot';
