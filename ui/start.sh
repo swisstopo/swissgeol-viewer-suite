9c08701ebafaad6c966503214e1322eb2d5ff584
Daniel von Atzigen <daniel@vatz.ch>
2025-08-12 11:48:38 +0200

    Modify docker to use local @swissgeol package

--- ui/start.sh
+++ ui/start.sh
@@ -6,5 +6,8 @@ npm ci --no-audit
 # Remove previous build output.
 rm -r dist/*
 
+rm -r /app/ui/node_modules/@swissgeol
+cp -r /app/ui/node_modules_bkp/@swissgeol /app/ui/node_modules/@swissgeol
+
 # Start the development server.
 npm run start
