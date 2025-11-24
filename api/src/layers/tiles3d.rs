d39b94e2da7b6e772aeb67f45c708e8a2e626516
Daniel von Atzigen <daniel.vonatzigen@ebp.ch>
2025-11-24 09:00:20 +0100

    Add shared order_of_properties

--- api/src/layers/tiles3d.rs
+++ api/src/layers/tiles3d.rs
@@ -1,5 +1,7 @@
-use crate::LayerSource;
+use anyhow::anyhow;
+use crate::{LayerSource};
 use serde::{Deserialize, Serialize};
+use crate::layers::config::{Parse, ParseContext};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(deny_unknown_fields, rename_all(serialize = "camelCase"))]
@@ -9,6 +11,45 @@ pub struct Tiles3dLayer {
 
     /// The order in which the layer's properties are sorted when displayed.
     /// Keys that are left out will be sorted below any sorted ones, in default order.
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    order_of_properties: Vec<String>,
+    #[serde(default, skip_serializing_if = "Tiles3dLayerOrderOfProperties::is_empty")]
+    order_of_properties: Tiles3dLayerOrderOfProperties,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Tiles3dLayerOrderOfProperties {
+    Reference(String),
+    Definition(Vec<String>),
+}
+
+impl Default for Tiles3dLayerOrderOfProperties {
+    #[inline]
+    fn default() -> Self {
+        Self::Definition(vec![])
+    }
+}
+
+impl Tiles3dLayerOrderOfProperties {
+    #[inline]
+    fn is_empty(&self) -> bool {
+        match self {
+            Tiles3dLayerOrderOfProperties::Reference(_) => false,
+            Tiles3dLayerOrderOfProperties::Definition(order) => order.is_empty(),
+        }
+    }
+}
+
+impl Parse for Tiles3dLayer {
+    fn parse(mut self, context: &mut ParseContext) -> anyhow::Result<Self> {
+        self.order_of_properties = match self.order_of_properties {
+            order@Tiles3dLayerOrderOfProperties::Definition(_) => order,
+            Tiles3dLayerOrderOfProperties::Reference(name) => {
+                let definition = context.config.order_of_properties.get(&name).ok_or_else(|| {
+                    anyhow!("[{}] Unknown order of properties: {name}", context.display)
+                })?;
+                Tiles3dLayerOrderOfProperties::Definition(definition.clone())
+            }
+        };
+        Ok(self)
+    }
 }
