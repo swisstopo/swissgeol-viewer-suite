40c5bd57c1ead6f84bb952ed29101d5457398df9
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2026-06-09 11:41:36 +0200

    feat: add layer source enum to backend model

--- api/src/layers/wmts.rs
+++ api/src/layers/wmts.rs
@@ -2,8 +2,23 @@ use crate::OgcSource;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum WmtsLayerSource {
+    #[serde(rename = "WMS")]
+    Wms,
+
+    #[serde(rename = "WMTS")]
+    Wmts,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(deny_unknown_fields, rename_all(serialize = "camelCase"))]
 pub struct WmtsLayer {
+    /// The source of the layer.
+    /// If absent, the layer is resolved from the default swisstopo WMS/WMTS capabilities
+    // For externally configured layers, this should be either "WMS" or "WMTS".
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub source: Option<WmtsLayerSource>,
+
     /// The zoom level (zoomed in) from which on no higher resolution tiles will be fetched.
     /// Instead, this level's tiles will be scaled up to fit higher zoom levels.
     #[serde(default, skip_serializing_if = "Option::is_none")]
