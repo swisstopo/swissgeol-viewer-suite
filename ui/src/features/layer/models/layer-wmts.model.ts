6818e3413f41f828a5273c7e71b302a64b7f5003
Volodymyr Hutsuliak <volodya1234321@gmail.com>
2026-06-19 11:40:31 +0200

    feat: update frontend wmts layer model

--- ui/src/features/layer/models/layer-wmts.model.ts
+++ ui/src/features/layer/models/layer-wmts.model.ts
@@ -21,13 +21,19 @@ export interface WmtsLayer extends BaseLayer {
   source: WmtsLayerSource;
 
   /**
+   * Optional service key used to resolve the WMS/WMTS capabilities endpoint.
+   * If omitted, the default maps.geo.admin service is used.
+   */
+  service?: string | null;
+
+  /**
    * The URL of the WMS/WMTS service used to render this layer.
    *
    * If absent, the default swisstopo service URL for the configured source is used.
    * For WMS sources, this is the service base URL.
    * For WMTS sources, this is the capabilities document URL.
    */
-  serviceUrl: string | null;
+  serviceUrl: string | null; // TODO remove
 
   /**
    * The zoom level (zoomed in) from which on no higher resolution tiles will be fetched.
